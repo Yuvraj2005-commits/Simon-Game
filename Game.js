e5d76e0cf00881ee0681012c0858208843766b38
Yuvraj Prakashh <yuvrajprakash4321@gmail.com>
2025-06-04 22:34:30 +0530

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,47 +1,39 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
-
-// var gamePattern = [];
-
-
+var gamePattern = [];
 var userClickedPattern = [];
-
 var level = 0;
 var started = false;
 
-function nextSequence(){
-
+function nextSequence() {
     level++;
     $("h1").text("Level " + level);
+    userClickedPattern = [];
+    
     var randnum = Math.random() * 4;
     randnum = Math.floor(randnum);
     console.log(randnum);
     
     var randomChosenColour = buttonColours[randnum];
-     
-    // gamePattern.push(randomChosenColour);
+    gamePattern.push(randomChosenColour);
     
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    playbeat(randomChosenColour);
     
     return randomChosenColour;
 }
 
-
-$(".btn").on("click", function(){
-   
+$(".btn").on("click", function() {
     var userChosenColour = $(this).attr("id");
     console.log(userChosenColour);
     
-   
     userClickedPattern.push(userChosenColour);
-   
     console.log(userClickedPattern);
     
     $("#" + userChosenColour).fadeOut(100).fadeIn(100);
     playbeat(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
 });
-
-
 
 $(document).keypress(function() {
     if (!started) {
@@ -49,21 +41,43 @@ $(document).keypress(function() {
         nextSequence();
         started = true;
     }
-})
+});
 
-function playbeat(userChosenColour){
-    var audio = new Audio("sounds/" + userChosenColour + ".mp3");
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+        
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("wrong");
+        playbeat("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
+function playbeat(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
-function animatePress(cureentcolor){
-    $("#"+cureentcolor).addClass("pressed");
-    setTimeout(function(){
-        $("#"+cureentcolor).removeClass("pressed");
-    },100);
-}                       
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
 
-// This duplicate button click handler can be removed since we already have the .btn handler above
-// $("button").click(function(){
-//     console.log("clicked");
-// });
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
